00319334fa252c89a28cda540e31d53771a7936a
dan-ghita <dan_ghita_95@yahoo.com>
2016-04-02 14:49:18 +0300

    Innitial project skeleton

---
+++ app/models/question.server.model.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * Module dependencies.
+ */
+var mongoose = require('mongoose'),
+	Schema = mongoose.Schema;
+
+/**
+ * Question Schema
+ */
+var QuestionSchema = new Schema({
+	// Question model fields   
+	// ...
+});
+
+mongoose.model('Question', QuestionSchema);
